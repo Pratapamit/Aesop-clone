104dd9ead24a0b430c33651fc4f41e9abf56a781
Amit Pratap Singh <pratapamit3000@gmail.com>
2021-10-16 19:44:18 +0530

    removed unneccessory files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import TopSection from './Components/TopSection';
 import BottomSection from './Components/BottomSection';
 import Medicine from './Components/Medicine';
-import BottomSection2 from './Components/BottomSection';
 import Footer from './Components/Footer';
 import MeddleSection from './Components/MeddleSection';
 import MeddleSection2 from './Components/MeddleSection2';
